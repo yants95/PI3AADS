ddca5c1f53d5228c91d5c70edf7be02f7fd69b85
Yan Soares <yts.1995@gmail.com>
2018-04-18 11:41:58 -0300

    adicionada relacionamento com tabela de parametros

--- app/Aquario.php
+++ app/Aquario.php
@@ -3,10 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Parametros;
 
 class Aquario extends Model
 {
     protected $fillable = [
         'data_montagem', 'largura', 'altura', 'comprimento', 'descricao','valor', 'data_desmontagem'
     ];
+
+    public function parametros() {
+    	return $this->hasMany(Parametros::class);
+    }
+
 }
