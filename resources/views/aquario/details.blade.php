d2277a1b0f357b14e0f61a3e97a11a8b04bb9783
Yan Soares <yts.1995@gmail.com>
2018-04-18 19:30:06 -0300

    modal de detalhes finalizado

--- resources/views/aquario/details.blade.php
+++ resources/views/aquario/details.blade.php
@@ -17,41 +17,41 @@
                     <div class="col-md-12">
                         <div class="col-sm-4">
                             <label for="data_montagem">Data de montagem:</label> 
-                            <label id="dt_montagem" id="dt_montagem" name="dt_montagem" style="font-weight: normal;"></label>
+                            <label id="dt_montagem" name="dt_montagem" style="font-weight: normal;"></label>
                         </div>  
 
                         <div class="col-sm-4">
                             <label for="largura">Largura:</label> 
-                            <label id="aq_largura" id="aq_largura" name="aq_largura" style="font-weight: normal;"></label>
+                            <label id="aq_largura" name="aq_largura" style="font-weight: normal;"></label>
                         </div>  
 
                         <div class="col-sm-4">
                             <label for="data_montagem">Comprimento:</label> 
-                            <label id="aq_comprimento" id="aq_comprimento" name="aq_comprimento" style="font-weight: normal;"></label>
+                            <label id="aq_comprimento" name="aq_comprimento" style="font-weight: normal;"></label>
                         </div>
                     </div>
                     
                     <div class="col-md-12"><br>
                         <div class="col-sm-4">
                             <label for="altura">Altura:</label> 
-                            <label id="aq_altura" id="aq_altura" name="aq_altura" style="font-weight: normal;"></label>
+                            <label id="aq_altura" name="aq_altura" style="font-weight: normal;"></label>
                         </div>
 
                         <div class="col-sm-4">
                             <label for="descricao">Descrição:</label> 
-                            <label id="aq_descricao" id="aq_descricao" name="aq_descricao" style="font-weight: normal;"></label>
+                            <label id="aq_descricao" name="aq_descricao" style="font-weight: normal;"></label>
                         </div>
 
                         <div class="col-sm-4">
                             <label for="valor">Valor:</label> 
-                            <label id="aq_valor" id="aq_valor" name="aq_valor" style="font-weight: normal;"></label>
+                            <label id="aq_valor" name="aq_valor" style="font-weight: normal;"></label>
                         </div>
                     </div>
 
                     <div class="col-md-12"><br>
                         <div class="col-sm-4">
                             <label for="data_desmontagem">Data de desmontagem:</label> 
-                            <label id="dt_desmontagem" id="dt_desmontagem" name="dt_desmontagem" style="font-weight: normal;"></label>
+                            <label id="dt_desmontagem" name="dt_desmontagem" style="font-weight: normal;"></label>
                         </div>
                     </div>
 
