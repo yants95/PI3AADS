89117a0a47254ca91b12d69d7a70670299b4e8fb
Yan Soares <yts.1995@gmail.com>
2018-04-18 19:29:35 -0300

    listagem de parametros do aquario - ja relacionado

--- resources/views/aquario/parameters.blade.php
+++ resources/views/aquario/parameters.blade.php
@@ -8,7 +8,7 @@
             integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
             crossorigin="anonymous"></script>
 
-    <script src="js/crud.js"></script>
+    <script src="../../js/crud-parameters.js"></script>
 
 @stop
 
@@ -39,11 +39,35 @@
                     <td>{{ $param->nitrito }}</td>
                     <td>{{ $param->aquario->descricao }}</td>
                     <td>
-                    <button class="edit-modal btn btn-primary">
+                    <button class="edit-modal btn btn-primary"      data-param="{{$param->data}}"
+                                                                    data-ph="{{$param->ph}}"
+                                                                    data-salinidade="{{$param->salinidade}}"
+                                                                    data-calcio="{{$param->calcio}}"
+                                                                    data-magnesio="{{$param->magnesio}}"
+                                                                    data-reserva_alcalina="{{$param->reserva_alcalina}}"
+                                                                    data-amonia="{{$param->amonia}}"
+                                                                    data-nitrito="{{$param->nitrito}}"
+                                                                    data-nitrato="{{$param->nitrato}}"
+                                                                    data-fosfato="{{$param->fosfato}}"
+                                                                    data-silica="{{$param->silica}}"
+                                                                    data-toggle="modal"
+                                                                    data-target="#editParameters">
                         <span class="glyphicon glyphicon-edit"></span> Editar
                     </button>
 
-                    <button class="details-modal btn btn-info">
+                    <button class="details-modal btn btn-info"      data-param="{{$param->data}}"
+                                                                    data-ph="{{$param->ph}}"
+                                                                    data-salinidade="{{$param->salinidade}}"
+                                                                    data-calcio="{{$param->calcio}}"
+                                                                    data-magnesio="{{$param->magnesio}}"
+                                                                    data-reserva_alcalina="{{$param->reserva_alcalina}}"
+                                                                    data-amonia="{{$param->amonia}}"
+                                                                    data-nitrito="{{$param->nitrito}}"
+                                                                    data-nitrato="{{$param->nitrato}}"
+                                                                    data-fosfato="{{$param->fosfato}}"
+                                                                    data-silica="{{$param->silica}}"
+                                                                    data-toggle="modal"
+                                                                    data-target="#detailsParameters">
                         <span class="glyphicon glyphicon-th-list"></span> Detalhes
                     </button>
                 </td>
@@ -55,5 +79,7 @@
             
         </div>
     </div>
+    @include ('parameters.edit')
     @include ('parameters.create')
+    @include ('parameters.details')
 @stop
