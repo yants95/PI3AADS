04760729f8f88f94814866dfde70ab2d86b0d185
Yan Soares <yts.1995@gmail.com>
2018-04-18 11:47:05 -0300

    criacao do modal de detalhes do aquario

--- resources/views/aquario/details.blade.php
+++ resources/views/aquario/details.blade.php
@@ -17,41 +17,41 @@
                     <div class="col-md-12">
                         <div class="col-sm-4">
                             <label for="data_montagem">Data de montagem:</label> 
-                            <input type="date" id="dt_montagem" id="dt_montagem" name="dt_montagem" class="form-control" disabled>
+                            <label id="dt_montagem" id="dt_montagem" name="dt_montagem" style="font-weight: normal;"></label>
                         </div>  
 
                         <div class="col-sm-4">
-                            <label for="largura">Largura</label>
-                            <input type="text" id="aq_largura" name="aq_largura" class="form-control" disabled>
-                        </div>
+                            <label for="largura">Largura:</label> 
+                            <label id="aq_largura" id="aq_largura" name="aq_largura" style="font-weight: normal;"></label>
+                        </div>  
 
                         <div class="col-sm-4">
-                            <label for="comprimento">Comprimento</label>
-                            <input type="text" id="aq_comprimento" name="aq_comprimento" class="form-control" disabled>
+                            <label for="data_montagem">Comprimento:</label> 
+                            <label id="aq_comprimento" id="aq_comprimento" name="aq_comprimento" style="font-weight: normal;"></label>
                         </div>
                     </div>
                     
                     <div class="col-md-12"><br>
                         <div class="col-sm-4">
-                            <label for="altura">Altura</label>
-                            <input type="text" id="aq_altura" name="aq_altura" class="form-control" disabled>
+                            <label for="altura">Altura:</label> 
+                            <label id="aq_altura" id="aq_altura" name="aq_altura" style="font-weight: normal;"></label>
                         </div>
 
                         <div class="col-sm-4">
-                            <label for="descricao">Descrição</label>
-                            <input type="text" id="aq_descricao" name="aq_descricao" class="form-control" disabled>
+                            <label for="descricao">Descrição:</label> 
+                            <label id="aq_descricao" id="aq_descricao" name="aq_descricao" style="font-weight: normal;"></label>
                         </div>
 
-                         <div class="col-sm-4">
-                            <label for="valor">Valor</label>
-                            <input type="text" id="aq_valor" name="aq_valor" class="form-control" disabled>
+                        <div class="col-sm-4">
+                            <label for="valor">Valor:</label> 
+                            <label id="aq_valor" id="aq_valor" name="aq_valor" style="font-weight: normal;"></label>
                         </div>
                     </div>
 
                     <div class="col-md-12"><br>
                         <div class="col-sm-4">
-                            <label for="data_desmontagem">Data de desmontagem</label>
-                            <input type="date" id="dt_desmontagem" name="dt_desmontagem" class="form-control" disabled>
+                            <label for="data_desmontagem">Data de desmontagem:</label> 
+                            <label id="dt_desmontagem" id="dt_desmontagem" name="dt_desmontagem" style="font-weight: normal;"></label>
                         </div>
                     </div>
 
