4a79238b8673a82939d978a20790616aeff22641
Yan Soares <yts.1995@gmail.com>
2018-04-18 21:31:02 -0300

    controller do grafico

--- app/Http/Controllers/AquarioGraficoController.php
+++ app/Http/Controllers/AquarioGraficoController.php
@@ -8,8 +8,14 @@ use Charts;
 class AquarioGraficoController extends Controller
 {
     public function index() {
-    	
+    	$chart = Charts::create('bar', 'highcharts')
+	        ->title('Produtos mais vendidos') // Título do gráfico
+	        ->labels(['Smartphone', 'Notebook', 'TV']) // Propriedades que vão ser adicionadas
+	        ->values([100, 20, 30]) // Valores das propriedades
+	        ->dimensions(500, 300) // Dimensão = 500 largura x 300 altura
+	        ->responsive(false) // É utilizado para se adaptar ao tamanho do box que se encontra
+	        ->elementLabel("Total de vendas"); // Legenda para o gráfico
     
-    return view('grafico.index'); 
+    	return view('grafico.index', ['chart' => $chart]); 
     }
 }
