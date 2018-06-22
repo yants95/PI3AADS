d7b05e8f02a0d4ec199abf1490595b54f061c34d
Yan Soares <yts.1995@gmail.com>
2018-06-22 10:29:29 -0300

    testando consumo de api

--- resources/views/aquario/geral.blade.php
+++ resources/views/aquario/geral.blade.php
@@ -9,14 +9,14 @@
 @section('content')
     <div class="row">
 
-    @foreach ($temperatura as $obj)
+    @foreach ($api->posts as $obj)
         <div class="col-md-3 col-sm-6 col-xs-12">
           <div class="info-box">
            <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
          
             <div class="info-box-content">
-              <span class="info-box-text">{{ $obj->name }}</span>
-              <span class="info-box-number"><small>{{ $obj->email }}</small></span>
+              <span class="info-box-text">Temperatura</span>
+              <span class="info-box-number"><small>{{ $obj->title }}</small></span>
             </div>
             <!-- /.info-box-content -->
           </div>
@@ -33,5 +33,31 @@
         <!-- /.col -->
       @endforeach
       </div>
+      @foreach ($api->comments as $obj)
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+           <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
+         
+            <div class="info-box-content">
+              <span class="info-box-text">Temperatura</span>
+              <span class="info-box-number"><small>{{ $obj->body }}</small></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        
+        <!-- /.col -->
+
+        <!-- fix for small devices only -->
+        
+        <!-- /.col -->
+        
+        <!-- /.col -->
+      @endforeach
+      </div>
+      
+      
 
 @stop
