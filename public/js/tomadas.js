e5669183c8dcef2592b5ab6df97a9d4651fa883f
Yan Soares <yts.1995@gmail.com>
2018-06-21 21:43:31 -0300

    integracao de tomadas

---
+++ public/js/tomadas.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+	console.log("teste");
+	
+	var url_final = "http://10.0.0.2/01/";
+	
+	$("#tomada1").on("change", function() {
+		if($("#tomada1").val() != "") {
+			url_final += $("#tomada1").val();
+			url(url_final);
+		}
+	});
+	
+	$("#tomada2").on("change", function() {
+		if($("#tomada2").val() != "") {
+			url_final += $("#tomada2").val();
+			url(url_final);
+		}
+	});
+	
+	$("#tomada3").on("change", function() {
+		if($("#tomada3").val() != "") {
+			url_final += $("#tomada3").val();
+			url(url_final);
+		}
+	});
+	
+	$("#tomada4").on("change", function() {
+		if($("#tomada4").val() != "") {
+			url_final += $("#tomada4").val();
+			url(url_final);
+		}
+	});
+	
+	$("#tomada5").on("change", function() {
+		if($("#tomada5").val() != "") {
+			url_final += $("#tomada5").val();
+			url(url_final);
+		}
+	});
+	
+	function url(a) {
+		console.log(a);
+	}
+	
+	$("#btEnviar").on("click", function() {
+		$.ajax({
+			  type: 'POST'
+			  ,url: url_final,
+			  //,dataType : 'html',
+			  beforeSend: function(){
+				if(typeof(url_final) !== undefined && url_final.length == 24){
+					
+				} else {
+					console.log(url_final);
+					console.log("TESTE");
+					return false;
+				}
+		      },
+			  success: function(response){
+				  console.log(response);
+			    preventDefault();
+			  },
+			  error: function(error, er){
+			   // console.log(error);
+			  }
+			});
+	});
+	
+	
+
+	
+});
