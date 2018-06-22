dfae004f00eafff5d9a549e35268d823f4ae39d7
Yan Soares <yts.1995@gmail.com>
2018-06-22 11:35:56 -0300

    testando consumo de api

--- resources/views/aquario/geral.blade.php
+++ resources/views/aquario/geral.blade.php
@@ -22,41 +22,11 @@
           </div>
           <!-- /.info-box -->
         </div>
-        <!-- /.col -->
         
-        <!-- /.col -->
-
-        <!-- fix for small devices only -->
         
-        <!-- /.col -->
-        
-        <!-- /.col -->
       @endforeach
       </div>
-      @foreach ($api->comments as $obj)
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-           <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
-         
-            <div class="info-box-content">
-              <span class="info-box-text">Temperatura</span>
-              <span class="info-box-number"><small>{{ $obj->body }}</small></span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-        
-        <!-- /.col -->
-
-        <!-- fix for small devices only -->
-        
-        <!-- /.col -->
-        
-        <!-- /.col -->
-      @endforeach
-      </div>
+      
       
       
 
