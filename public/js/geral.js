e8e9a062d81bf7a07771167631c151461d850a57
Yan Soares <yts.1995@gmail.com>
2018-06-23 14:48:13 -0300

    consumo de API e integração com boia

--- public/js/geral.js
+++ public/js/geral.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 	$.getJSON("https://my-json-server.typicode.com/typicode/demo/db", function(dados) {
-
         if (!("erro" in dados)) {
             //Atualiza os campos com os valores da consulta.
             $("#tempAtual").text(dados.posts[0].id);
@@ -8,4 +7,30 @@ $(document).ready(function() {
             $("#tempMaxima").text(dados.posts[2].id);
         }
     });
+	
+	$("#desativarControle").on("click", function() {
+		$.ajax({
+			url: "http://10.0.0.2/03/0",
+			type: 'GET',
+			success: function(data) {
+				console.log(data);
+			},
+			error: function(data) {
+				console.log(data);
+			}
+		});
+	});
+	
+	$("#ativarControle").on("click", function() {
+		$.ajax({
+			url: "http://10.0.0.2/03/1",
+			type: 'GET',
+			success: function(data) {
+				console.log(data);
+			},
+			error: function(data) {
+				console.log(data);
+			}
+		});
+	});
 });
