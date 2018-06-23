0985023985878547ff0edda0f9e716a52b3360d5
Yan Soares <yts.1995@gmail.com>
2018-06-23 12:43:25 -0300

    mudando dashboard

--- resources/views/aquario/geral.blade.php
+++ resources/views/aquario/geral.blade.php
@@ -4,29 +4,83 @@
 
 @section('content_header')
     <h1>PA</h1>
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+            crossorigin="anonymous"></script>
+    <script type="text/javascript" src="{{ URL::asset('js/geral.js') }}"></script>
 @stop
 
 @section('content')
     <div class="row">
-
-    @foreach ($api->posts as $obj)
-        <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="col-md-4">
           <div class="info-box">
-           <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
+           <span class="info-box-icon bg-yellow"><i class="ion ion-ios-gear-outline"></i></span>
          
             <div class="info-box-content">
-              <span class="info-box-text">Temperatura</span>
-              <span class="info-box-number"><small>{{ $obj->title }}</small></span>
+              <span class="info-box-text">Temperatura atual</span>
+              <span class="info-box-number" id="tempAtual"><small></small></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          
+          <div class="info-box">
+           <span class="info-box-icon bg-yellow"><i class="ion ion-ios-gear-outline"></i></span>
+         
+            <div class="info-box-content">
+              <span class="info-box-text">Temperatura mínima</span>
+              <span class="info-box-number" id="tempMinima"><small></small></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+          
+          <div class="info-box">
+           <span class="info-box-icon bg-yellow"><i class="ion ion-ios-gear-outline"></i></span>
+         
+            <div class="info-box-content">
+              <span class="info-box-text">Temperatura máxima</span>
+              <span class="info-box-number" id="tempMaxima"><small></small></span>
             </div>
             <!-- /.info-box-content -->
           </div>
           <!-- /.info-box -->
         </div>
         
-        
-      @endforeach
+        <div class="col-md-4">
+          <div class="info-box">
+           <span class="info-box-icon bg-yellow"><i class="ion ion-ios-gear-outline"></i></span>
+         
+            <div class="info-box-content">
+              <span class="info-box-text">Comentários</span>
+              <span class="info-box-number" id="comentarios"><small></small></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          
+          <div class="info-box">
+           <span class="info-box-icon bg-yellow"><i class="ion ion-ios-gear-outline"></i></span>
+         
+            <div class="info-box-content">
+              <span class="info-box-text">Temperatura mínima</span>
+              <span class="info-box-number" id="tempMinima"><small></small></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+          
+          <div class="info-box">
+           <span class="info-box-icon bg-yellow"><i class="ion ion-ios-gear-outline"></i></span>
+         
+            <div class="info-box-content">
+              <span class="info-box-text">Temperatura máxima</span>
+              <span class="info-box-number" id="tempMaxima"><small></small></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
       </div>
-      
       
       
 
