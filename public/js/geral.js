26425fa8689c07cb50fc7043161905f3f1bf5abb
Yan Soares <yts.1995@gmail.com>
2018-06-23 15:45:53 -0300

    atribuindo data e hora às divs

--- public/js/geral.js
+++ public/js/geral.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
-	$.getJSON("https://my-json-server.typicode.com/typicode/demo/db", function(dados) {
+	$("#dataArduino").text("23/06/2018");
+	$("#horaArduino").text("15:12");
+	
+	/*$.getJSON("https://my-json-server.typicode.com/typicode/demo/db", function(dados) {
         if (!("erro" in dados)) {
             //Atualiza os campos com os valores da consulta.
             $("#tempAtual").text(dados.posts[0].id);
             $("#tempMinima").text(dados.posts[1].id);
             $("#tempMaxima").text(dados.posts[2].id);
         }
-    });
+    });*/
 	
 	$("#desativarControle").on("click", function() {
 		$.ajax({
