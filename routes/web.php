6098c7e774e4f33eacd8b3d978ed3d710cbe3876
Yan Soares <yts.1995@gmail.com>
2018-06-22 23:29:15 -0300

    adicionada rota para temperatura

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,14 @@ Route::get('/', function () {
 });
 
 $this->group(['middleware' => 'auth'], function () {
-    $this->get('/home', 'AquarioController@get_temperatura');
+    $this->get('/home', 'AquarioController@api');
     $this->get('/aquario', 'AquarioController@index');
     $this->get('/aquario/inserir', 'AquarioController@create')->name('create.aquario');
     $this->post('/aquario/inserir', 'AquarioController@store')->name('add.aquario');
     $this->post('/aquario/edit', 'AquarioController@update');
     $this->get('/aquario/tomadas', 'AquarioController@tomadas');
     $this->get('/aquario/arduino', 'AquarioController@arduino')->name('show.arduino');
+    $this->get('/aquario/temperatura', 'AquarioController@temperatura')->name('show.temperatura');
     $this->post('/aquario/arduino', 'AquarioController@store_arduino')->name('add.arduino');
 
 
