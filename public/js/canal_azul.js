dd071918151757ca53dc8310496b5de2ee91d7bc
Filipe Natanael <filipenatanael@live.com>
2018-06-24 13:58:25 -0300

    Criando metodo para enviar requisição

--- public/js/canal_azul.js
+++ public/js/canal_azul.js
@@ -3,8 +3,7 @@ $(document).ready(function(){
 	var base_url = "http://10.0.0.2/05/";
 
 	$("#EnviarCincoPrimeiros").on("click", function() {
-		let url_final = validarParamentro(
-										base_url,
+		let paramentros = validarParamentro(
 										"Primeiro Horario.",
 										$.trim($("#pHorario1").val()),
 										$.trim($("#pPotencia1").val()),
@@ -18,23 +17,41 @@ $(document).ready(function(){
 										$.trim($("#pPotencia5").val())
 									);
 
-		console.log(url_final);
-
+		enviarRequisicao(base_url, paramentros);
 	});
 
 });
-
 
 /*
 Funcão para validar valores passado nos inputs
 
 @method validarParamentro(params..., mensagem)
-@return retorna um url montada para requisição
+@return retorna string de paramentros validados
 */
-function validarParamentro(base_url, mensagem, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10) {
+function validarParamentro(mensagem, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10) {
 	if (p1 && p2 && p3 && p4 && p5 && p6 && p7 && p8 && p9 && p10) {
-		return `${base_url}0${p1}0${p2}0${p3}0${p4}0${p5}0${p6}0${p7}0${p8}0${p9}0${p10}`;
+		return `0${p1}0${p2}0${p3}0${p4}0${p5}0${p6}0${p7}0${p8}0${p9}0${p10}`;
 	} else {
 		alert(`Preenchar todos os campos para o ${mensagem}`);
 	}
+}
+
+/*
+Funcão para realizar requisição
+
+@method validarParamentro(url, paramentros)
+*/
+function enviarRequisicao(url, paramentros) {
+	// monstar url final para requisição
+	let url_final = url+paramentros;
+	$.ajax({
+		url: url_final,
+		type: 'GET',
+		success: function(data) {
+			console.log(data);
+		},
+		error: function(error) {
+			console.log(error);
+		}
+	});
 }
