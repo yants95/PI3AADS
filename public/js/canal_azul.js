25cb0cd54b068ef94c23cd895203eb71692ec760
Filipe Natanael <filipenatanael@live.com>
2018-06-24 13:46:51 -0300

    Montando URL Final

--- public/js/canal_azul.js
+++ public/js/canal_azul.js
@@ -3,20 +3,22 @@ $(document).ready(function(){
 	var base_url = "http://10.0.0.2/05/";
 
 	$("#EnviarCincoPrimeiros").on("click", function() {
-		enviarDadosParaAPI(
-			base_url,
-			"Primeiro Horario.",
-			$.trim($("#pHorario1").val()),
-			$.trim($("#pPotencia1").val()),
-			$.trim($("#pHorario2").val()),
-			$.trim($("#pPotencia2").val()),
-			$.trim($("#pHorario3").val()),
-			$.trim($("#pPotencia3").val()),
-			$.trim($("#pHorario4").val()),
-			$.trim($("#pPotencia4").val()),
-			$.trim($("#pHorario5").val()),
-			$.trim($("#pPotencia5").val())
-		);
+		let url_final = validarParamentro(
+										base_url,
+										"Primeiro Horario.",
+										$.trim($("#pHorario1").val()),
+										$.trim($("#pPotencia1").val()),
+										$.trim($("#pHorario2").val()),
+										$.trim($("#pPotencia2").val()),
+										$.trim($("#pHorario3").val()),
+										$.trim($("#pPotencia3").val()),
+										$.trim($("#pHorario4").val()),
+										$.trim($("#pPotencia4").val()),
+										$.trim($("#pHorario5").val()),
+										$.trim($("#pPotencia5").val())
+									);
+
+		console.log(url_final);
 
 	});
 
@@ -25,11 +27,13 @@ $(document).ready(function(){
 
 /*
 Funcão para validar valores passado nos inputs
-enviarDadosParaAPI(params..., mensagem)
+
+@method validarParamentro(params..., mensagem)
+@return retorna um url montada para requisição
 */
-function enviarDadosParaAPI(base_url, mensagem, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10) {
+function validarParamentro(base_url, mensagem, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10) {
 	if (p1 && p2 && p3 && p4 && p5 && p6 && p7 && p8 && p9 && p10) {
-		console.log("ok");
+		return `${base_url}0${p1}0${p2}0${p3}0${p4}0${p5}0${p6}0${p7}0${p8}0${p9}0${p10}`;
 	} else {
 		alert(`Preenchar todos os campos para o ${mensagem}`);
 	}
