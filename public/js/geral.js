9b1bfcf5b2ebc41ebf31238fba992bc69cbdcceb
Yan Soares <yts.1995@gmail.com>
2018-06-23 22:48:17 -0300

    trabalhando status de controles

--- public/js/geral.js
+++ public/js/geral.js
@@ -9,6 +9,16 @@ $(document).ready(function() {
             $("#tempMinima").text(dados.posts[1].id);
             $("#tempMaxima").text(dados.posts[2].id);
         }
+        
+        if(dados.controles[0].nivel == "Desativado") {
+        	$("#nivelControle).removeClass("btn btn-danger");
+        	$("#nivelControle).addClass("btn btn-success");
+        	$("#nivelControle).text("Ativar nível de controle de água");
+        } else if((dados.controles[0].nivel == "Ativado") {
+        	$("#nivelControle).removeClass("btn btn-success");
+        	$("#nivelControle).addClass("btn btn-danger");
+        	$("#nivelControle).text("Desativar nível de controle de água");
+        }
     });*/
 	
 	$("#desativarControle").on("click", function() {
