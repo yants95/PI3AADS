da9b5b9bf61a6d942ae95f298a6b30f30b48e2af
Yan Soares <yts.1995@gmail.com>
2018-06-30 17:36:39 -0300

    usando server fake para consumir api de teste

--- public/js/geral.js
+++ public/js/geral.js
@@ -1,24 +1,26 @@
 $(document).ready(function() {
-	$("#dataArduino").text("23/06/2018");
-	$("#horaArduino").text("15:12");
 	
-	$.getJSON("https://github.com/typicode/json-server/blob/master/db.json", function(dados) {
+	
+	$.getJSON("http://my-json-server.typicode.com/yants95/PI3AADS/aquario", function(dados) {
+		headers: { "Access-Control-Allow-Origin","*"}
         if (!("erro" in dados)) {
             //Atualiza os campos com os valores da consulta.
         	console.log(dados);
-            $("#tempAtual").text(dados.aquario.temperatura[0].atual);
-            $("#tempMinima").text(dados.posts[1].id);
-            $("#tempMaxima").text(dados.posts[2].id);
+        	$("#dataArduino").text(dados[0].dateTime[0].data);
+        	$("#horaArduino").text(dados[0].dateTime[0].hora);
+            $("#tempAtual").text(dados[0].temperatura[0].atual);
+            $("#tempMinima").text(dados[0].temperatura[0].minima);
+            $("#tempMaxima").text(dados[0].temperatura[0].maxima);
             
-            $("#tomada1").text(dados.tomadas[0].t1);
-            $("#tomada2").text(dados.tomadas[1].t2);
-            $("#tomada3").text(dados.tomadas[2].t3);
-            $("#tomada4").text(dados.tomadas[3].t4);
-            $("#tomada5").text(dados.tomadas[4].t5);
+            $("#tomada1").text(dados[0].tomadas[0].t1);
+            $("#tomada2").text(dados[0].tomadas[0].t2);
+            $("#tomada3").text(dados[0].tomadas[0].t3);
+            $("#tomada4").text(dados[0].tomadas[0].t4);
+            $("#tomada5").text(dados[0].tomadas[0].t5);
             
-            $("#controle_nivel").text(dados.tomadas[0].nivel);
-            $("#controle_temp").text(dados.tomadas[1].temperatura);
-            $("#controle_iluminacao").text(dados.tomadas[2].iluminacao);
+            $("#controle_nivel").text(dados[0].controles[0].nivel);
+            $("#controle_temp").text(dados[0].controles[0].temperatura);
+            $("#controle_iluminacao").text(dados[0].controles[0].iluminacao);
         }
         
         
