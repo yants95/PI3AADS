8d2dec98336041a1a33767ad3e5d4a90fed5a4d6
Yan Soares <yts.1995@gmail.com>
2018-06-30 13:25:49 -0300

    atualização

--- public/js/crud.js
+++ public/js/crud.js
@@ -39,7 +39,7 @@ $('.modal-footer').on('click', '.edit', function(e) {
           'data_desmontagem': $('#data_desmontagem').val()
       },
       success: function(data) {
-          window.location.href="/aquario";
+    	  
       }
   });
 });
