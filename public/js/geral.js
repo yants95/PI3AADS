3b278e95651b17d838048fad2abf8464a8ba9690
Bruno Magalhães <bruno.magalhaes.parreira@gmail.com>
2018-07-03 16:00:33 -0300

    Correções e finalizações do dashboard.

--- public/js/geral.js
+++ public/js/geral.js
@@ -6,20 +6,25 @@ $(document).ready(function() {
             //Atualiza os campos com os valores da consulta.
             $("#dataArduino").text(dados.aquario[0].dateTime[0].data);
 	    $("#horaArduino").text(dados.aquario[0].dateTime[0].hora);
-            $("#tempAtual").text(dados.aquario[0].temperatura[0].atual);
-            //falta criar $("#tempIdeal").text(dados.aquario[0].temperatura[0].ideal);
+            
+	    $("#tempAtual").text(dados.aquario[0].temperatura[0].atual);
+            $("#tempIdeal").text(dados.aquario[0].temperatura[0].ideal);
 	    $("#tempMinima").text(dados.aquario[0].temperatura[0].minima);
             $("#tempMaxima").text(dados.aquario[0].temperatura[0].maxima);
-            //falta criar $("#potAzul").text(dados.aquario[0].potenciaAtual[0].azul);	
-            //falta criar $("#potBranco").text(dados.aquario[0].potenciaAtual[0].branco);	
-            $("#tomada1").text(dados.aquario[0].tomadas[0].t1);
+	    $("#tomadaAquecimento").text(dados.aquario[0].temperatura[0].aquecimento);
+            $("#tomadaResfriamento").text(dados.aquario[0].temperatura[0].resfriamento);
+            
+	    $("#potAzul").text(dados.aquario[0].potenciaAtual[0].azul);	
+            $("#potBranco").text(dados.aquario[0].potenciaAtual[0].branco);	
+            
+	    $("#tomada1").text(dados.aquario[0].tomadas[0].t1);
             $("#tomada2").text(dados.aquario[0].tomadas[0].t2);
             $("#tomada3").text(dados.aquario[0].tomadas[0].t3);
             $("#tomada4").text(dados.aquario[0].tomadas[0].t4);
             $("#tomada5").text(dados.aquario[0].tomadas[0].t5);
             
             $("#controle_nivel").text(dados.aquario[0].controles[0].nivel);
-            //falta criar $("#controle_repos").text(dados.aquario[0].controles[0].qtdReposicoes);	
+            $("#controle_repos").text(dados.aquario[0].controles[0].qtdReposicoes);	
             $("#controle_temp").text(dados.aquario[0].controles[0].temperatura);
             $("#controle_iluminacao").text(dados.aquario[0].controles[0].iluminacao);
         }
@@ -43,6 +48,32 @@ $(document).ready(function() {
 	$("#ativarControle").on("click", function() {
 		$.ajax({
 			url: "http://10.0.0.2/03/1",
+			type: 'GET',
+			success: function(data) {
+				console.log(data);
+			},
+			error: function(data) {
+				console.log(data);
+			}
+		});
+	});
+
+	$("#desativarControleTemp").on("click", function() {
+		$.ajax({
+			url: "http://10.0.0.2/02/0&0&0",
+			type: 'GET',
+			success: function(data) {
+				console.log(data);
+			},
+			error: function(data) {
+				console.log(data);
+			}
+		});
+	});
+	
+	$("#ativarControleTemp").on("click", function() {
+		$.ajax({
+			url: "http://10.0.0.2/02/200&200&200",
 			type: 'GET',
 			success: function(data) {
 				console.log(data);
--- resources/views/aquario/geral.blade.php
+++ resources/views/aquario/geral.blade.php
@@ -12,100 +12,188 @@
 <p style="font-weight: bold; font-size: 20px;">Data: <label id="dataArduino"></label> -  Hora: <label id="horaArduino"></p>
 
 <p style="font-weight: bold; font-size: 20px;">TEMPERATURA</p>
+    <div class="row">	
+       <div class="box-footer">
+    	  <button class="btn btn-success" id="ativarControleTemp">Ativar controle de iluminação</button>
+    	  <button class="btn btn-danger" id="desativarControleTemp">Desativar controle de iluminação</button>
+       </div><br>
 
-    <div class="row">
+	<div class="col-md-4 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-green"><i class="fa fa-desktop"></i></span>
+            <div class="info-box-content">
+              <span class="info-box-text">Controle de Temperatura</span>
+              <span class="info-box-number" id="controle_temp"><small></small></span>
+            </div>
+          </div>
+        </div>
+
         <div class="col-md-4 col-sm-6 col-xs-12">
           <div class="info-box">
             <span class="info-box-icon bg-aqua"><i class="fa fa-thermometer-half"></i></span>
-
             <div class="info-box-content">
               <span class="info-box-text">Temperatura atual</span>
               <span class="info-box-number" id="tempAtual"><small></small></span>
             </div>
-            <!-- /.info-box-content -->
           </div>
-          <!-- /.info-box -->
         </div>
-        <!-- /.col -->
-        <div class="col-md-4 col-sm-6 col-xs-12">
+
+	<div class="col-md-4 col-sm-6 col-xs-12">
           <div class="info-box">
             <span class="info-box-icon bg-aqua"><i class="fa fa-thermometer-half"></i></span>
-
+            <div class="info-box-content">
+              <span class="info-box-text">Temperatura ideal</span>
+              <span class="info-box-number" id="tempIdeal"><small></small></span>
+            </div>
+          </div>
+        </div>	
+        
+	<div class="col-md-4 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-aqua"><i class="fa fa-thermometer-half"></i></span>
             <div class="info-box-content">
               <span class="info-box-text">Temperatura mínima</span>
               <span class="info-box-number" id="tempMinima"><small></small></span>
             </div>
-            <!-- /.info-box-content -->
           </div>
-          <!-- /.info-box -->
         </div>
         
         <div class="col-md-4 col-sm-6 col-xs-12">
           <div class="info-box">
             <span class="info-box-icon bg-aqua"><i class="fa fa-thermometer-half"></i></span>
-
             <div class="info-box-content">
               <span class="info-box-text">Temperatura máxima</span>
               <span class="info-box-number" id="tempMaxima"><small></small></span>
             </div>
-            <!-- /.info-box-content -->
           </div>
-          <!-- /.info-box -->
         </div>
+	
+        <div class="col-md-4 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-yellow"><i class="fa fa-plug"></i></span>
+            <div class="info-box-content">
+              <span class="info-box-text">Aquecimento</span>
+              <span class="info-box-number" id="tomadaAquecimento"><small></small></span>
+            </div>
+          </div>
         </div>
+
+        <div class="col-md-4 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-yellow"><i class="fa fa-plug"></i></span>
+            <div class="info-box-content">
+              <span class="info-box-text">Resfriamento</span>
+              <span class="info-box-number" id="tomadaResfriamento"><small></small></span>
+            </div>
+          </div>
+        </div>
+</div><br>
         
+<p style="font-weight: bold; font-size: 20px;">ILUMINAÇÃO</p>
+        <div class="row">
+	    <div class="box-footer">
+              <button class="btn btn-success" id="ativarIluminacaoAutomatica">Ativar iluminação automática</button>
+              <button class="btn btn-success" id="ativarIluminacaoManual">Ativar iluminacão manual</button>
+            </div><br>
+	<div class="col-md-4 col-sm-6 col-xs-12">
+	  <div class="info-box">
+	    <span class="info-box-icon bg-green"><i class="fa fa-desktop"></i></span>
+	    <div class="info-box-content">
+	      <span class="info-box-text">Iluminação</span>
+	      <span class="info-box-number" id="controle_iluminacao"><small></small></span>
+	    </div>
+	  </div>
+	</div>
+
+	    <div class="col-md-4 col-sm-6 col-xs-12">
+	      <div class="info-box">
+		<span class="info-box-icon bg-blue"><i class="fa fa-plug"></i></span>
+		<div class="info-box-content">
+		  <span class="info-box-text">Potência atual canal Azul</span>
+		  <span class="info-box-number" id="potAzul"><small></small></span>
+		</div>
+	      </div>
+	    </div>
+
+            <div class="col-md-4 col-sm-6 col-xs-12">
+              <div class="info-box">
+                <span class="info-box-icon bg-white"><i class="fa fa-plug"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Potência atual canal Branco</span>
+                  <span class="info-box-number" id="potBranco"><small></small></span>
+                </div>
+              </div>
+	    </div>
+</div><br>
+        
+<p style="font-weight: bold; font-size: 20px;">REPOSIÇÃO DE ÁGUA DOCE</p>
+    <div class="row">
+	<div class="box-footer">
+    	  <button class="btn btn-success" id="ativarControle">Ativar controle de nível de água</button>
+    	  <button class="btn btn-danger" id="desativarControle">Desativar controle de nível de água</button>
+    	</div><br>
+
+        <div class="col-md-4 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-green"><i class="fa fa-desktop"></i></span>
+            <div class="info-box-content">
+              <span class="info-box-text">Controle de nível de água</span>
+              <span class="info-box-number" id="controle_nivel"><small></small></span>
+            </div>
+          </div>
+        </div>
+	
+	<div class="col-md-4 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-green"><i class="fa fa-desktop"></i></span>
+            <div class="info-box-content">
+              <span class="info-box-text">Número de reposições</span>
+              <span class="info-box-number" id="controle_repos"><small></small></span>
+            </div>
+          </div>
+        </div>
+    </div><br>        
+
 <p style="font-weight: bold; font-size: 20px;">TOMADAS</p>
     <div class="row">
         <div class="col-md-4 col-sm-6 col-xs-12">
           <div class="info-box">
             <span class="info-box-icon bg-yellow"><i class="fa fa-plug"></i></span>
-
             <div class="info-box-content">
               <span class="info-box-text">Tomada 1</span>
               <span class="info-box-number" id="tomada1"><small></small></span>
             </div>
-            <!-- /.info-box-content -->
           </div>
-          <!-- /.info-box -->
         </div>
-        <!-- /.col -->
+
         <div class="col-md-4 col-sm-6 col-xs-12">
           <div class="info-box">
             <span class="info-box-icon bg-yellow"><i class="fa fa-plug"></i></span>
-
             <div class="info-box-content">
               <span class="info-box-text">Tomada 2</span>
               <span class="info-box-number" id="tomada2"><small></small></span>
             </div>
-            <!-- /.info-box-content -->
           </div>
-          <!-- /.info-box -->
         </div>
         
         <div class="col-md-4 col-sm-6 col-xs-12">
           <div class="info-box">
             <span class="info-box-icon bg-yellow"><i class="fa fa-plug"></i></span>
-
             <div class="info-box-content">
               <span class="info-box-text">Tomada 3</span>
               <span class="info-box-number" id="tomada3"><small></small></span>
             </div>
-            <!-- /.info-box-content -->
           </div>
-          <!-- /.info-box -->
         </div>
         
         <div class="col-md-4 col-sm-6 col-xs-12">
           <div class="info-box">
             <span class="info-box-icon bg-yellow"><i class="fa fa-plug"></i></span>
-
             <div class="info-box-content">
               <span class="info-box-text">Tomada 4</span>
               <span class="info-box-number" id="tomada4"><small></small></span>
             </div>
-            <!-- /.info-box-content -->
           </div>
-          <!-- /.info-box -->
         </div>
         
         <div class="col-md-4 col-sm-6 col-xs-12">
@@ -116,92 +204,8 @@
               <span class="info-box-text">Tomada 5</span>
               <span class="info-box-number" id="tomada5"><small></small></span>
             </div>
-            <!-- /.info-box-content -->
           </div>
-          <!-- /.info-box -->
         </div>
-        </div>
-        
-<p style="font-weight: bold; font-size: 20px;">POTÊNCIA ATUAL ILUMINAÇÃO</p>
-        <div class="row">
-            <div class="col-md-4 col-sm-6 col-xs-12">
-              <div class="info-box">
-                <span class="info-box-icon bg-blue"><i class="fa fa-plug"></i></span>
-    
-                <div class="info-box-content">
-                  <span class="info-box-text">Canal Azul</span>
-                  <span class="info-box-number" id="potAzul"><small></small></span>
-                </div>
-                <!-- /.info-box-content -->
-              </div>
-              <!-- /.info-box -->
-            </div>
-            <!-- /.col -->
-            <div class="col-md-4 col-sm-6 col-xs-12">
-              <div class="info-box">
-                <span class="info-box-icon bg-white"><i class="fa fa-plug"></i></span>
-    
-                <div class="info-box-content">
-                  <span class="info-box-text">Canal Branco</span>
-                  <span class="info-box-number" id="potBranco"><small></small></span>
-                </div>
-                <!-- /.info-box-content -->
-              </div>
-              <!-- /.info-box -->
-		   </div>
-	</div>
-        
-        
-
-<p style="font-weight: bold; font-size: 20px;">CONTROLES</p>
-    <div class="row">
-        <div class="col-md-4 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-green"><i class="fa fa-desktop"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Nível</span>
-              <span class="info-box-number" id="controle_nivel"><small></small></span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-        <div class="col-md-4 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-green"><i class="fa fa-desktop"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Temperatura</span>
-              <span class="info-box-number" id="controle_temp"><small></small></span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        
-        <div class="col-md-4 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-green"><i class="fa fa-desktop"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Iluminação</span>
-              <span class="info-box-number" id="controle_iluminacao"><small></small></span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        
-              
-      
-      <div class="box-footer">
-      	<button class="btn btn-danger" id="desativarControle">Desativar controle de nível de água</button><br><br>
-      	<button class="btn btn-success" id="ativarControle">Ativar controle de nível de água</button>
-        <button class="btn btn-success" id="ativarIluminacaoAutomatica>Ativar iluminação automática</button>
-        <button class="btn btn-success" id="ativarIluminacaoManual">Ativar iluminacão manual</button>
-      </div>
-      
+</div>
 
 @stop
