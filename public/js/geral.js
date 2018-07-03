90da765deba72baa497d0d60586ca28e832db372
Bruno Magalhães <bruno.magalhaes.parreira@gmail.com>
2018-07-03 10:05:45 -0300

    Correção do JSON.

--- public/js/geral.js
+++ public/js/geral.js
@@ -4,21 +4,22 @@ $(document).ready(function() {
 	$.getJSON("http://10.0.0.2/00", function(dados) {
         if (!("erro" in dados)) {
             //Atualiza os campos com os valores da consulta.
-        	$("#dataArduino").text(dados.aquario.dateTime[0].data);
-        	/*$("#horaArduino").text(dados.aquario.dateTime[0].hora);
-            $("#tempAtual").text(dados[0].temperatura[0].atual);
-            $("#tempMinima").text(dados[0].temperatura[0].minima);
-            $("#tempMaxima").text(dados[0].temperatura[0].maxima);
+        	    $("#dataArduino").text(dados.aquario[0].dateTime[0].data);
+	    $("#horaArduino").text(dados.aquario[0].dateTime[0].hora);
+            $("#tempAtual").text(dados.aquario[0].temperatura[0].atual);
+            //$("#tempIdeal").text(dados.aquario[0].temperatura[0].ideal);
+	    $("#tempMinima").text(dados.aquario[0].temperatura[0].minima);
+            $("#tempMaxima").text(dados.aquario[0].temperatura[0].maxima);
             
-            $("#tomada1").text(dados[0].tomadas[0].t1);
-            $("#tomada2").text(dados[0].tomadas[0].t2);
-            $("#tomada3").text(dados[0].tomadas[0].t3);
-            $("#tomada4").text(dados[0].tomadas[0].t4);
-            $("#tomada5").text(dados[0].tomadas[0].t5);
+            $("#tomada1").text(dados.aquario[0].tomadas[0].t1);
+            $("#tomada2").text(dados.aquario[0].tomadas[0].t2);
+            $("#tomada3").text(dados.aquario[0].tomadas[0].t3);
+            $("#tomada4").text(dados.aquario[0].tomadas[0].t4);
+            $("#tomada5").text(dados.aquario[0].tomadas[0].t5);
             
-            $("#controle_nivel").text(dados[0].controles[0].nivel);
-            $("#controle_temp").text(dados[0].controles[0].temperatura);
-            $("#controle_iluminacao").text(dados[0].controles[0].iluminacao);*/
+            $("#controle_nivel").text(dados.aquario[0].controles[0].nivel);
+            $("#controle_temp").text(dados.aquario[0].controles[0].temperatura);
+            $("#controle_iluminacao").text(dados.aquario[0].controles[0].iluminacao);
         }
         
         
